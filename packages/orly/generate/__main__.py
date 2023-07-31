413203d22a4601da1fb8d1e05c2230fd3473dbdb
Neill Robson <neill.robson@pendo.io>
2023-07-31 15:50:29 -0400

    Update constants to match frontend preview

--- packages/orly/generate/__main__.py
+++ packages/orly/generate/__main__.py
@@ -2,11 +2,11 @@ from generate import generate_image
 
 
 def main(event):
-    title = event.get("title", "Title Text Goes Here")
-    topText = event.get("top_text", "This is where top text goes")
-    author = event.get("author", "Kanstantsin Tsedryk")
-    image_code = event.get("image_code", "7")
-    theme = event.get("theme", "3")
+    title = event.get("title", "Title Goes Here")
+    topText = event.get("top_text", "This is top text.")
+    author = event.get("author", "Kanye West")
+    image_code = event.get("image_code", "2")
+    theme = event.get("theme", "1")
     guide_text_placement = event.get("guide_text_placement", "bottom_right")
     guide_text = event.get("guide_text", "The Definitive Guide")
 
